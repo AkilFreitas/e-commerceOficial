c3f8d73f8f7f9887f61cdc26a5b3d409e3e48e40
Paula Cavalcante <akilfreitas@gmail.com>
2018-05-26 09:17:41 -0300

    Produto

--- src/main/negocio/Produto.java
+++ src/main/negocio/Produto.java
@@ -1,13 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Val
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import negocio.Categoria;
+import negocio.Tamanho;
+import negocio.Fornecedor;
+@Entity
 public class Produto {
     
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(length = 20)
+    private String nome;
+    @Column(length = 20)
+    private String marca;
+    @Column(length = 20)
+    Categoria categoria;
+    @Column(length = 20)
+    private String descricao;
+    @Column(length = 20)
+    private float preco;
+    @Column(length = 20)
+    private String imagem;
+    @Column(length = 20)
+    Tamanho tamanho;
+    @Column(length = 20)
+    private Boolean disponivel;
+    @Column(length = 20)
+    private boolean promocao;
+    @Column(length = 20)
+    Fornecedor fornecedor;
+
+    public Produto() {
+    }
 }
