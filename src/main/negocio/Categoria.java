124b78b69dd991995bcde78472993ba2dfdf5a60
PauloNeto01 <paulo-neto2012@outlook.com>
2018-05-25 22:14:29 -0300

    Adição do arquivo persistence xml nao configurado

--- src/main/negocio/Categoria.java
+++ src/main/negocio/Categoria.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
 @Entity
 public class Categoria{
+    
     @Id
     @GeneratedValue
     private int id;
